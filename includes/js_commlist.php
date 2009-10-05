80bfa8f324c0356c0b0815663831c75ef7431586
Alexander Kuzmin <roosit@abricos.org>
2009-10-05 11:25:52 +0000

    Импорт модулей

---
+++ includes/js_commlist.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!Brick::$session->IsAdminMode()){	return; }
+
+$brick = Brick::$builder->brick;
+$json = Brick::$input->clean_gpc('p', 'json', TYPE_STR);
+$obj = json_decode($json);
+if (empty($obj->page)){ $obj->page = 1; }
+
+if ($obj->act == 'status'){
+	CMSQComt::SpamSet(Brick::$db, $obj);
+}
+
+$brick->param->var['total'] = CMSQComt::AdmListCount(Brick::$db);
+$brick->param->var['page'] = $obj->page;
+
+$rows = CMSQComt::AdmList(Brick::$db, $obj->page);
+
+while (($row = Brick::$db->fetch_array($rows))){
+	$brick->param->var['lst'] .= str_replace("#c#", 
+		json_encode_ext($row), 
+		$brick->param->var['i']
+	);
+}
+
+?>
